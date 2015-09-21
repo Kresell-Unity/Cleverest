3ef20381b020872e73132b886f29a24d86516f43
Messering <messering@yandex.ru>
2015-09-22 01:31:09 +0300

    DataBase- connect db

--- Assets/Cleverest/Scripts/db/db_con.cs
+++ Assets/Cleverest/Scripts/db/db_con.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
-
 using System.Collections.Generic;
+using System;
+using System.Data;
 using Mono.Data.Sqlite;
 
 public class Players
 {
+
 	public int Id{ get; set; }
 	public string Name_Player{ get; set;}
 	public float Score{ get; set; }
@@ -20,16 +22,41 @@ public class QA
 }
 
 public class db_con : MonoBehaviour {
-
+	private string connectionString;
 	// Use this for initialization
 	void Start () {
-		using (var db=new SqliteConnection(Application.dataPath+"/Cleverest/db/cleverest.sqlite")) {
-			//IEnumerable<Players> list=db.Query<Players>("SELECT * FROM weapons");
-		}
+		connectionString = "URI=file:" + Application.dataPath + "/Cleverest/db/cleverest.sqlite";
+		players ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-}
+
+	private void players(){
+		using(IDbConnection dbConnection=new SqliteConnection(connectionString))
+		      {
+			dbConnection.Open();
+
+				using(IDbCommand dbCmd=dbConnection.CreateCommand())
+			{
+				string sqlQuery="SELECT * FROM Players";
+				dbCmd.CommandText= sqlQuery;
+
+				using(IDataReader reader=dbCmd.ExecuteReader()){
+					while(reader.Read()){
+						Debug.Log(reader.GetString(0));
+						Debug.Log (reader.GetFloat(1));
+						//dbConnection.Close();
+						//reader.Close();
+					}
+				}
+			
+			}
+			
+		}
+		}
+		}
+
+
